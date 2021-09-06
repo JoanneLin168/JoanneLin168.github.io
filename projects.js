1779f0a66697f501c7496075b1fa34a045f51d1a
Joanne <yulin20028@hotmail.co.uk>
2021-09-06 20:12:49 +0100

    Updated visuals

--- projects.js
+++ projects.js
@@ -1,13 +1,6 @@
 allProjects = {
     "complete" : [
         {
-            "title": "Revize",
-            "text": "Created a website using PHP that provides study tools to help mainly A-Level Students to study for their exams.",
-            "link": "http://revize.altervista.org/",
-            "image": "images/ReVize.png",
-            "date": "January 2019 - March 2019"
-        },
-        {
             "title": "Cards Against Humanity Remake Website",
             "text": "Created a website using ASP.NET Core MVC Framework to play Cards Against Humanity with friends.",
             "link": "https://cahremake.azurewebsites.net/",
@@ -31,7 +24,7 @@ allProjects = {
         {
             "title": "Covid Tracker",
             "text": "Creating a website that allows users to view the trends of coronavirus around the globe.",
-            "link": "https://github.com/JoanneLin168/pongML",
+            "link": "https://joannelin168.github.io/COVIDTracker.html",
             "image": "images/COVIDTracker.png",
             "date": "January 2021"
         }
@@ -48,56 +41,52 @@ allProjects = {
     ]
 }
 
-/*
-<div class="col-md-4">
-    <div class="card" style="width: 18rem;">
-        <img src="images/mario.png" class="card-img-top" alt="mario.png">
-        <div class="card-body">
-            <h5 class="card-title">marioML</h5>
-            <div class="accordion" id="accordion6">
-                <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapse6" aria-expanded="true" aria-controls="collapse">
-                    Click here to read more
-                </button>
-                </div>
-                <div id="collapse6" class="collapse" aria-labelledby="heading6" data-parent="#accordion6">
-                    <p class="card-text">Working with a friend to create a machine learning project that can tell if an image is Mario or not</p>
-                    <a href="https://github.com/JoanneLin168/marioML" class="card-link">Link here!</a>
-                </div>
-            <p class="card-text"><small class="text-muted">December 2020</small></p>
-            
-        </div>
-    </div>
-</div>
-*/
+showcase = [
+    {
+        "title": "Online Whiteboard",
+        "text": "Creating a website that allows multiple users to draw on a whiteboard together",
+        "link": "http://jyl-online-whiteboard.herokuapp.com/",
+        "image": "images/whiteboard.png",
+        "date": "September 2021"
+    },
+    {
+        "title": "Cards Against Humanity Remake Website",
+        "text": "Created a website using ASP.NET Core MVC Framework to play Cards Against Humanity with friends.",
+        "link": "https://cahremake.azurewebsites.net/",
+        "image": "images/CAHR.png",
+        "date": "May 2020 - August 2020"
+    },
+    {
+        "title": "Pong Machine Learning",
+        "text": "Created a machine-learning program that will self teach how to play pong. Produced graphs at the end of the program.",
+        "link": "https://github.com/JoanneLin168/pongML",
+        "image": "images/pongML.png",
+        "date": "January 2021"
+    },
+]
 
 complete = allProjects.complete.reverse();
 incomplete = allProjects.incomplete.reverse();
 
 function displayProjects(type, projects) {
-    var displayArea;
-    displayArea = document.getElementById(type); // type is a string
+    var displayArea = document.getElementById(type); // type is a string
 
     var index = 0;
-    var rowCounter = -1;
+    
+    var row = document.createElement("div");
+    row.className = "row";
+    row.id = type+"_row";
+    displayArea.appendChild(row);
+    
     while (index < projects.length) {
-        if (index == 0 || (index % 4 == 1 && index != 1)) {
-            rowCounter++;
-            var row = document.createElement("div");
-            row.className = "row";
-            row.id = type+"_row_"+rowCounter;
-            displayArea.appendChild(row);
-        }
-
-        var row = document.getElementById(type+"_row_"+rowCounter);
         row.innerHTML += `
-        <div class="col-md-4">
+        <div class="col-sm-4">
             <div class="card" style="width: 18rem;">
-                <img src="`+projects[index].image+`" class="card-img-top" alt="Missing image">
+                <a href="`+projects[index].link+`" target="_blank"><img src="`+projects[index].image+`" class="card-img-top" alt="Missing image"></a>
                 <div class="card-body">
                     <h5 class="card-title">`+projects[index].title+`</h5>
                     <p class="card-text">`+projects[index].text+`</p>
                     <p class="card-text"><small class="text-muted">`+projects[index].date+`</small></p>
-                    <a href="`+projects[index].link+`" class="btn btn-primary">Link here!</a>
                 </div>
             </div>
         </div>
@@ -107,5 +96,31 @@ function displayProjects(type, projects) {
     }
 }
 
-displayProjects("complete", complete);
-displayProjects("incomplete", incomplete);
+function displayShowcase() {
+    var displayArea = document.getElementById("showcase");
+
+    var index = 0;
+    var row = document.createElement("div");
+    row.className = "row";
+    displayArea.appendChild(row);
+    
+    while (index < showcase.length) {
+        row.innerHTML += `
+        <div class="col-sm-4">
+            <div class="card mb-3">
+                <a href="`+showcase[index].link+`" target="_blank"><img src="`+showcase[index].image+`" class="card-img-top" alt="Missing image"></a>
+                <div class="card-body">
+                    <h5 class="card-title">`+showcase[index].title+`</h5>
+                    <p class="card-text">`+showcase[index].text+`</p>
+                    <p class="card-text"><small class="text-muted">`+showcase[index].date+`</small></p>
+                </div>
+            </div>
+        </div>
+        `;
+        
+        index++;
+    }
+}
+
+
+
