19dfa0649f9c33cfb406308b3998487eead671f9
Joanne <yulin20028@hotmail.co.uk>
2021-12-24 20:52:07 +0000

    Attempted modals

--- js/projects.js
+++ js/projects.js
@@ -127,6 +127,25 @@ function displayShowcase() {
                     <h5 class="card-title">`+showcase[index].title+`</h5>
                     <p class="card-text">`+showcase[index].text+`</p>
                     <p class="card-text"><small class="text-muted">`+showcase[index].date+`</small></p>
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal`+index+`">More details</button>
+                </div>
+            </div>
+        </div>
+        <div class="modal fade" id="modal`+i+`" tabindex="-1" role="dialog" aria-labelledby="modal`+i+`Label" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="modal`+i+`Label">`+showcase[index].title+`</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        ...
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+                    </div>
                 </div>
             </div>
         </div>
